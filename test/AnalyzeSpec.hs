98b6adcf4a17f695b21129faad3d8b5491969044
Eugene <e@egeek.me>
2022-06-18 08:54:46 +0300

    Test `draftDuration` when PR is not merged

--- test/AnalyzeSpec.hs
+++ test/AnalyzeSpec.hs
@@ -13,7 +13,7 @@ import EventType
 
 spec :: Spec
 spec =
-  describe "draftDuration" $
+  describe "draftDuration" $ do
     context "when PR is merged" $ do
       it "returns zero when there are no events" $ do
         pull <- defaultDDI
@@ -59,6 +59,30 @@ spec =
         let pull' = pull { fEvents = events }
         draftDuration pull' `shouldBe` (1 + 0.5 + 1.5) * nominalDay
 
+      it "calculates duration from draft to merged" $ do
+        -- even though it doesn't make much sense according to github UI
+        draftTime <- utcTime "2022-01-06T00:00:00Z"
+        pull <- defaultDDI
+        let pull' = pull { fEvents = [markDraftEvent draftTime] }
+        draftDuration pull' `shouldBe` 2 * nominalDay
+
+    context "when PR is not merged" $ do
+      it "returns zero when there are no events" $ do
+        pull <- defaultUnmergedDDI
+        draftDuration pull `shouldBe` 0
+
+      it "excludes duration from draft to unmerged" $ do
+        times <- traverse utcTime -- pairs of undraft-draft times
+          [ "2022-01-02T00:00:00Z" -- 1 day
+          , "2022-01-02T06:00:00Z"
+          , "2022-01-02T18:00:00Z" -- 0.5 days
+          , "2022-01-04T12:00:00Z"
+          ]
+        let events = zipWith ($) (cycle [markReadyEvent, markDraftEvent]) times
+        pull <- defaultUnmergedDDI
+        let pull' = pull { fEvents = events }
+        draftDuration pull' `shouldBe` (1 + 0.5) * nominalDay
+
 defaultDDI :: MonadFail m => m FreeDDI
 defaultDDI = do
   fCreated <- utcTime "2022-01-01T00:00:00Z"
@@ -68,6 +92,11 @@ defaultDDI = do
     , fMerged
     , fEvents = []
     }
+
+defaultUnmergedDDI :: MonadFail m => m FreeDDI
+defaultUnmergedDDI = do
+  x <- defaultDDI
+  pure $ x { fMerged = Nothing }
 
 markDraftEvent :: UTCTime -> PullEvent
 markDraftEvent time = PullEvent
