5496e4f98bdf52cf3146462d95c29e88c7a83a08
Eugene <e@egeek.me>
2022-06-18 08:19:38 +0300

    Fix warnings in `Spec`

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,8 +1,8 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeApplications #-}
 
 import Data.Functor.Identity
 import Data.Time
-import Data.Time.Format.ISO8601
 import Test.Hspec
 
 import Lib
@@ -28,31 +28,31 @@ main = hspec $ do
   describe "unfoldrM" $ do
     it "returns the first value" $ do
       let f = Identity . const ([0, 1], Nothing)
-      runIdentity (unfoldrM f 0) `shouldBe` [0, 1]
+      runIdentity (unfoldrM f (0 :: Int)) `shouldBe` [0, 1 :: Int]
 
     it "returns the combined value" $ do
       let f x = Identity ("ab", if x <= 1 then Just (x + 1) else Nothing)
-      runIdentity (unfoldrM f 0) `shouldBe` "ababab"
+      runIdentity (unfoldrM f (0 :: Int)) `shouldBe` ("ababab" :: String)
 
     it "combines values to the right" $ do
       let f x = Identity ([x, x + 1], if x <= 3 then Just (x + 2) else Nothing)
-      runIdentity (unfoldrM f 0) `shouldBe` [0, 1, 2, 3, 4, 5]
+      runIdentity (unfoldrM f 0) `shouldBe` [0, 1, 2, 3, 4, 5 :: Int]
 
   describe "diffWorkTime" $ do
     it "calculates time difference within work week" $ do
       monday <- utcTime "2022-01-03T10:00:00Z"
       friday <- utcTime "2022-01-07T22:00:00Z"
-      diffWorkTime' friday monday `shouldBe` mkNumberDays 4.5
+      diffWorkTime' friday monday `shouldBe` mkNumberDays @Float 4.5
 
     it "ignores full weekends" $ do
       friday <- utcTime "2022-01-07T12:00:00Z"
       nextMonday <- utcTime "2022-01-10T12:00:00Z"
-      diffWorkTime' nextMonday friday `shouldBe` mkNumberDays 1
+      diffWorkTime' nextMonday friday `shouldBe` mkNumberDays @Int 1
 
     it "ignores multiple full weekends" $ do
       friday <- utcTime "2021-12-31T12:00:00Z"
       laterMonday <- utcTime "2022-02-07T12:00:00Z"
-      diffWorkTime' laterMonday friday `shouldBe` mkNumberDays 26
+      diffWorkTime' laterMonday friday `shouldBe` mkNumberDays @Int 26
 
     it "calculates non-integer days diff" $ do
       thursday <- utcTime "2022-01-06T10:00:00Z"
@@ -68,7 +68,7 @@ newtype NumberDays = NumberDays WorkDiffTime
   deriving Eq
 
 instance Show NumberDays where
-  show (NumberDays x) = (show . realToFrac . (/ nominalDay) . unWorkDiffTime $ x) ++ " days"
+  show (NumberDays x) = (show @Float . realToFrac . (/ nominalDay) . unWorkDiffTime $ x) ++ " days"
 
 mkNumberDays :: Real a => a -> NumberDays
 mkNumberDays = NumberDays . WorkDiffTime . (* nominalDay) . realToFrac
