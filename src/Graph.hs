e914651608ac0f98440b76185da98201f6eba056
Eugene <e@egeek.me>
2023-01-16 12:50:47 +0200

    Produce a bigger graph
    This wasn't easy to figure out.

--- src/Graph.hs
+++ src/Graph.hs
@@ -16,10 +16,13 @@ import Lib
 
 plotOpenTimes :: FilePath -> [(Sprint, NominalDiffTime)] -> IO ()
 plotOpenTimes filepath times = logResult <=< file filepath
-  . texts . setTicks $ plot (enumFromTo 0 $ length times - 1) (snd <$> times)
+  . setFigureSize . texts . setTicks $ plot (enumFromTo 0 $ length times - 1) (snd <$> times)
 
   where
     logResult = either (hPutStrLn stderr) (\s -> unless (null s) $ putStrLn s)
+
+    -- setting dpi doesn't work at all; setting figsize globally via `setParameter` doesn't work
+    setFigureSize = (# "; fig.set(figwidth=10.24, figheight=7.68)")
 
     setTicks p = p # ";f=plot.gca();f.axes.xaxis.set_ticks(" # ticks # ", labels=[" # labels # "], rotation=10);f.axes.yaxis.set_ticklabels([])"
       where
